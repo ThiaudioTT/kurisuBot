dab107754b5150b1479a62b04b094514e43b07f7
ThiaudioTT <thiago.sky25@gmail.com>
2024-03-19 16:30:39 -0300

    style: format code

--- kurisubot.cpp
+++ kurisubot.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
 #include <dpp/dpp.h>
+#include <iostream>
 #define PROJECT_NAME "kurisuBot"
 
 std::string BOT_TOKEN = "";
 
-int main(const int argc, const char**argv)
-{
-    if (argc > 2) {
-        std::cerr << "Usage: " << argv[0] << " [bot token]\n";
-        return 1;
-    }
+int main(const int argc, const char **argv) {
+  if (argc > 2) {
+    std::cerr << "Usage: " << argv[0] << " [bot token]\n";
+    return 1;
+  }
 
-    // Set bot token
-    if(argc == 2) {
-        BOT_TOKEN = argv[1];
-        std::cout << "Successfully set bot token" << std::endl;
+  // Set bot token
+  if (argc == 2) {
+    BOT_TOKEN = argv[1];
+    std::cout << "Successfully set bot token" << std::endl;
+  } else {
+    // get token from env.json
+    std::cout << "No token provided, attempting to read from env.json"
+              << std::endl;
+    std::ifstream env_file("env.json");
+    if (env_file.is_open()) {
+      nlohmann::json env;
+      env_file >> env;
+      BOT_TOKEN = env["token"];
+      std::cout << "Successfully read token from env.json" << std::endl;
     } else {
-        // get token from env.json
-        std::cout << "No token provided, attempting to read from env.json" << std::endl;
-        std::ifstream env_file("env.json");
-        if(env_file.is_open()) {
-            nlohmann::json env;
-            env_file >> env;
-            BOT_TOKEN = env["token"];
-            std::cout << "Successfully read token from env.json" << std::endl;
-        } else {
-            std::cerr << "No token provided and failed to read from env.json" << std::endl;
-            return 1;
-        }
+      std::cerr << "No token provided and failed to read from env.json"
+                << std::endl;
+      return 1;
     }
+  }
 
-    std::cout << "This is project " << PROJECT_NAME << ".\n";
+  std::cout << "This is project " << PROJECT_NAME << ".\n";
 
-    dpp::cluster bot(BOT_TOKEN);
+  dpp::cluster bot(BOT_TOKEN);
 
-    bot.on_log(dpp::utility::cout_logger());
+  bot.on_log(dpp::utility::cout_logger());
 
-    bot.on_slashcommand([](const dpp::slashcommand_t &event)
-                        {
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        } });
+  bot.on_slashcommand([](const dpp::slashcommand_t &event) {
+    if (event.command.get_command_name() == "ping") {
+      event.reply("Pong!");
+    }
+  });
 
-    bot.on_ready([&bot](const dpp::ready_t &event)
-                 {
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
-        } });
+  bot.on_ready([&bot](const dpp::ready_t &event) {
+    if (dpp::run_once<struct register_bot_commands>()) {
+      bot.global_command_create(
+          dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+    }
+  });
 
-    bot.start(dpp::st_wait);
+  bot.start(dpp::st_wait);
 
-    return 0;
+  return 0;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ```
 meson compile -C builddir && ./builddir/kurisubot
 ```
+
+uses clang format to format the code
+```bash
+clang-format -i src/*.cpp
+```
