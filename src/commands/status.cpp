fe72e703dd3d4458140b39be8e740d30a31a195b
ThiaudioTT <thiago.sky25@gmail.com>
2024-03-22 12:25:16 -0300

    feat(status): implements CPU usage

--- src/commands/status.cpp
+++ src/commands/status.cpp
@@ -1,13 +1,57 @@
 #include "status.hpp"
 #include <fstream>
+#include <chrono>
+#include <thread>
 
 void Status::status(const dpp::slashcommand_t& event) {
-    event.reply ("I'm alive, Sir!\nRam Usage: " + getRamUsage());
+    event.reply ("I'm alive, Sir!\n**CPU**: " + getCPUUsage() + 
+                "\n**RAM**: " + getRamUsage());
+}
+
+
+std::vector<size_t> Status::getCpuJiffies() {
+    std::ifstream stat("/proc/stat");
+    stat.ignore(5, ' '); // ignore the first 5 characters "cpu  "
+    std::vector<size_t> cpu_times;
+
+    for(size_t time; stat >> time; cpu_times.push_back(time));
+
+    stat.close();
+
+    return cpu_times;
 }
 
 
 /**
  * @brief Returns the CPU usage of the system. Like: "6%"
+ * 
+ * @return std::string 
+ */
+std::string Status::getCPUUsage() {
+    //https://rosettacode.org/wiki/Linux_CPU_utilization
+    //https://support.checkpoint.com/results/sk/sk65143
+    // https://www.idnt.net/en-US/kb/941772
+
+    std::vector<size_t> cpu_jiffies1 = getCpuJiffies();
+    // sleep(1); // TODO: can we use other thing instead of sleep? This is blocking the whole program
+    // TODO: this is blocking the whole program, we should use async or threads. use event thinking or something
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::vector<size_t> cpu_jiffies2 = getCpuJiffies();
+
+    const size_t total_time1 = std::accumulate(cpu_jiffies1.begin(), cpu_jiffies1.end(), 0);
+    const size_t total_time2 = std::accumulate(cpu_jiffies2.begin(), cpu_jiffies2.end(), 0);
+
+    const size_t delta_time = total_time2 - total_time1;
+    const size_t cpu_idle = cpu_jiffies2[3] - cpu_jiffies1[3]; // idle time is the 4th element in the vector
+
+    const size_t cpu_used = delta_time - cpu_idle;
+
+    // calc the percentage
+    return std::to_string(cpu_used * 100 / delta_time) + "%";
+}
+
+/**
+ * @brief Returns the RAM usage of the system. Like: "6%"
  * 
  * Check https://www.baeldung.com/linux/proc-meminfo for more information
  * @return std::string 
@@ -38,7 +82,6 @@ std::string Status::getRamUsage() {
         // } else if (line.compare(0, 13, "MemAvailable:") == 0) {
         //     available = std::stol(line.substr(13));
         // }
-
         // if(total_mem && free_mem && buffers && cached) break; // break if we read all the values
 
         // we only need MemTotal, MemAvailable
@@ -47,6 +90,8 @@ std::string Status::getRamUsage() {
         } else if (line.compare(0, 13, "MemAvailable:") == 0) {
             available_mem = std::stol(line.substr(13));
         }
+
+        if(total_mem && available_mem) break; // break if we read all the values
     }
 
     meminfo.close();
--- src/commands/status.hpp
+++ src/commands/status.hpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <dpp/dpp.h>
+#include <vector>
 
+// todo: docs
 namespace Status {
     // main handler has the same name as the namespace
     void status(const dpp::slashcommand_t &event);
 
     // helper functions
     std::string getCPUUsage();
+    std::vector<size_t> getCpuJiffies();
     std::string getRamUsage();
     std::string getDiskUsage();
     std::string getUptime();
